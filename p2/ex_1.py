23d96acce3d557cc3cb70d5ba595d339f62c595a
bmajosek <bartek9568@gmail.com>
2025-10-21 00:42:48 +0200

    p2

---
+++ p2/ex_1.py
@@ -0,0 +1,29 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G1 = nx.karate_club_graph()
+
+G2 = nx.davis_southern_women_graph()
+
+G3 = nx.les_miserables_graph()
+
+G4 = nx.florentine_families_graph()
+
+G5 = nx.random_geometric_graph(50, 0.25)
+
+G6 = nx.krackhardt_kite_graph()
+
+graphs = [
+    ("Zachary's Karate Club", G1),
+    ("Davis Southern Women", G2),
+    ("Les Misérables", G3),
+    ("Florentine Families", G4),
+    ("Random Geometric Network", G5),
+    ("Krackhardt Kite", G6),
+]
+
+for name, G in graphs:
+    plt.figure(figsize=(7, 5))
+    nx.draw(G, with_labels=False, node_size=100, node_color='skyblue')
+    plt.title(f"{name}")
+    plt.show()
